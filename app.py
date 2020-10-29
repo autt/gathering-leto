5a47eb75dda44f5447e9813121bcebecae954ce7
Dag Rambraut <DAGRA@equinor.com>
2020-10-29 16:45:18 +0100

    Added logo and font from EDS

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ import dash_core_components as dcc
 from data.git_data import get_issues
 from data.mock_data import issues, tags
 
-app = dash.Dash(__name__)
+
+external_style =['https://eds-static.equinor.com/font/equinor-font.css']
+
+
+app = dash.Dash(__name__, external_stylesheets=external_style)
 
 
 data_issues = get_issues("equinor/gathering-leto")
@@ -17,6 +21,10 @@ def get_data_dict(data_json_list):
         d = {"title": node.title, "id": node.id, "user": node.user.login}
         results.append(d)
     return results
+
+
+def _create_logo():
+    return html.Img(src="https://eds-static.equinor.com/logo/equinor-logo-primary.svg#red", id="logo", width=100, alt="Equinor")
 
 
 def _create_table_view_div(data_dict):
@@ -46,6 +54,7 @@ def _create_tags_listbox_div(data_listbox):
 
 app.layout = html.Div(
     [
+        _create_logo(),
         _create_tags_listbox_div(tags),
         _create_table_view_div(get_data_dict(data_issues)),
     ]
